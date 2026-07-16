992d183830539d6c9cb56bce5706357102113a3b
doublehigh <ibrahimishaq4real2021@gmail.com>
2026-07-16 16:01:12 +0100

    Use ecommerce CSV seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(UserSeeder::class);
-        $this->call(AdminProfileSeeder::class);
-        $this->call(VendorShopProfileSeeder::class);
+        $this->call(EcommerceCsvSeeder::class);
     }
 }
