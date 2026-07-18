67dab87fa95245e70cbc8c20afab417b4632762f
doublehigh <ibrahimishaq4real2021@gmail.com>
2026-07-18 12:25:41 +0100

    Use CSS marquee for mobile categories

--- resources/views/frontend/home/sections/marketplace-shortcuts.blade.php
+++ resources/views/frontend/home/sections/marketplace-shortcuts.blade.php
@@ -3,13 +3,22 @@
         <div class="marketplace_shortcuts_inner">
             <div class="marketplace_shortcuts_panel category_panel">
                 <div class="marketplace_panel_title">Shop by category</div>
-                <div class="marketplace_category_grid">
-                    @foreach ($homeCategories as $category)
-                        <a href="{{route('products.index', ['category' => $category->slug])}}" class="marketplace_category_link">
-                            <i class="{{$category->icon ?: 'far fa-box'}}"></i>
-                            <span>{{$category->name}}</span>
-                        </a>
-                    @endforeach
+                <div class="marketplace_category_viewport">
+                    <div class="marketplace_category_grid">
+                        @foreach ($homeCategories as $category)
+                            <a href="{{route('products.index', ['category' => $category->slug])}}" class="marketplace_category_link">
+                                <i class="{{$category->icon ?: 'far fa-box'}}"></i>
+                                <span>{{$category->name}}</span>
+                            </a>
+                        @endforeach
+
+                        @foreach ($homeCategories as $category)
+                            <a href="{{route('products.index', ['category' => $category->slug])}}" class="marketplace_category_link marketplace_category_duplicate" aria-hidden="true" tabindex="-1">
+                                <i class="{{$category->icon ?: 'far fa-box'}}"></i>
+                                <span>{{$category->name}}</span>
+                            </a>
+                        @endforeach
+                    </div>
                 </div>
             </div>
 
@@ -38,57 +47,3 @@
         </div>
     </div>
 </section>
-
-@push('scripts')
-<script>
-    $(document).ready(function() {
-        const categoryRail = $('.marketplace_category_grid');
-
-        if (!categoryRail.length) {
-            return;
-        }
-
-        let categoryAutoScroll;
-        let resumeAutoScroll;
-
-        function stopCategoryAutoScroll() {
-            window.clearInterval(categoryAutoScroll);
-            window.clearTimeout(resumeAutoScroll);
-            categoryRail.stop(true);
-        }
-
-        function startCategoryAutoScroll() {
-            stopCategoryAutoScroll();
-
-            const rail = categoryRail.get(0);
-
-            if (window.innerWidth > 575 || rail.scrollWidth <= rail.clientWidth) {
-                return;
-            }
-
-            categoryAutoScroll = window.setInterval(function() {
-                const firstItem = categoryRail.find('.marketplace_category_link').first();
-                const itemGap = 10;
-                const scrollAmount = firstItem.length ? firstItem.outerWidth() + itemGap : 112;
-                const maxScroll = rail.scrollWidth - rail.clientWidth;
-                const isAtEnd = categoryRail.scrollLeft() >= maxScroll - 4;
-
-                categoryRail.animate({
-                    scrollLeft: isAtEnd ? 0 : Math.min(categoryRail.scrollLeft() + scrollAmount, maxScroll)
-                }, 650);
-            }, 2400);
-        }
-
-        function pauseCategoryAutoScroll() {
-            stopCategoryAutoScroll();
-            resumeAutoScroll = window.setTimeout(startCategoryAutoScroll, 3500);
-        }
-
-        categoryRail.on('touchstart pointerdown wheel', pauseCategoryAutoScroll);
-        $(window).on('resize load', startCategoryAutoScroll);
-
-        startCategoryAutoScroll();
-        window.setTimeout(startCategoryAutoScroll, 800);
-    });
-</script>
-@endpush
